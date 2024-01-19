a93a8a98d625eb617ce5e1632bb1cabd9673d1ab
Min Pyae <minmp2002@gmail.com>
2024-01-19 12:31:53 +0000

    Organized TopMoversView and AllCoinView Folder Structure

---
+++ CrytoLive/Views/AllCoinsView/AllCoinsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  AllCoinsRowView.swift
+//  CrytoLive
+//
+//  Created by Chris Min on 19/01/2024.
+//
+
+import SwiftUI
+
+struct AllCoinsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllCoinsRowView()
+}
---
+++ CrytoLive/Views/AllCoinsView/AllCoinsView.swift
@@ -0,0 +1,18 @@
+//
+//  AllCoinsView.swift
+//  CrytoLive
+//
+//  Created by Chris Min on 19/01/2024.
+//
+
+import SwiftUI
+
+struct AllCoinsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllCoinsView()
+}
