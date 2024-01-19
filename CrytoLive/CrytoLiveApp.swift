1feb56c944c0301ecc8c279561e598f7e5ff4cf3
Min Pyae <minmp2002@gmail.com>
2024-01-19 12:01:14 +0000

    Initial Commit

---
+++ CrytoLive/CrytoLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoLiveApp.swift
+//  CrytoLive
+//
+//  Created by Chris Min on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CrytoLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrytoLive/Views/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  CrytoLive
+//
+//  Created by Chris Min on 19/01/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // TOP MOVERS VIEW
+                
+                // ALL COINS VIEW
+                
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
